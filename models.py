3dfe99007b57274aca3a529c4a0689d1abe3bfdc
Dan1skk <makavelliawe@gmail.com>
2026-01-30 21:01:01 +0500

    переместил таблицу с рекордами в директорию data

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ import os
 
 class ScoreManager:
     def __init__(self):
-        self.file_path = "scores.csv"
+        self.file_path = "data/scores.csv"
         # Создаем файл с заголовками, если его нет
         if not os.path.exists(self.file_path):
             with open(self.file_path, "w", newline="", encoding="utf-8") as f:
