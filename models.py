60da913804909ca97333e7452bab5639fed219a3
Dan1skk <makavelliawe@gmail.com>
2026-02-01 15:16:11 +0500

    change data directory auto create

--- models.py
+++ models.py
@@ -1,9 +1,17 @@
 import csv
 import os
 
+
 class ScoreManager:
     def __init__(self):
-        self.file_path = "data/scores.csv"
+        # Путь к папке и файлу
+        self.directory = "data"
+        self.file_path = os.path.join(self.directory, "scores.csv")
+
+        # Создаем папку data, если её нет
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+
         # Создаем файл с заголовками, если его нет
         if not os.path.exists(self.file_path):
             with open(self.file_path, "w", newline="", encoding="utf-8") as f:
@@ -11,6 +19,7 @@ class ScoreManager:
                 writer.writerow(["Name", "Score", "Time"])
 
     def add_score(self, name, coins, time_spent):
+        """Добавляет новую запись в таблицу рекордов"""
         with open(self.file_path, "a", newline="", encoding="utf-8") as f:
             writer = csv.writer(f)
             writer.writerow([name, coins, time_spent])
